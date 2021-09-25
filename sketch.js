912f83a2e248ec624400578c146e68141db942dd
StandardProgrammer <89229682+StandardProgrammer@users.noreply.github.com>
2021-09-25 16:34:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup(){
 function draw() {
   background(180);
   rabbit.x = mouseX;
-  var select_sprites = Math.round(random(1, 3));
+  var select_sprites = Math.round(random(1, 4));
   if(frameCount%140 == 0){
   if(select_sprites == 1) {
       spawnRedImage();
